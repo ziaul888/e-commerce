46f6c7cc3d070f3e498a782fb3dc370c93bcc4e6
ziaul888 <ziaul935@gmail.com>
2021-01-19 04:13:31 +0600

    herocomponent

--- src/components/SideCart.jsx
+++ src/components/SideCart.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import {ProductConsumer} from "../context"
+import styled from "styled-components"
 
-export default function SideCart() {
+export default function CartPage() {
     return (
         <>
-          <h1>sidecart</h1>  
+           <ProductConsumer>
+           {value=>{   
+             const {cartOpen,cart,closeCart}=value;       
+             return (
+               <CartWrapper show={cartOpen} onClick={closeCart}>
+              <p> Cart items</p>
+               </CartWrapper>
+             )
+           }}
+         </ProductConsumer>
         </>
     )
 }
+
+
+
+const CartWrapper=styled.div`
+position:fixed;
+top:60px;
+right:0;
+width:100%;
+height:100%;
+background:var(--mainGrey);
+z-index:1;
+border-left:3px solid var(--primaryColor);
+transform: ${props => (props.show ? "translateX(0)" : "translateX(100%)")};
+transition:var(--mainTransition);
+@media (min-width:576px){
+  width:20rem;
+}
+`
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,38 +1,9 @@
 import React from 'react'
-import {ProductConsumer} from "../context"
-import styled from "styled-components"
 
 export default function CartPage() {
-    return (
-        <>
-           <ProductConsumer>
-           {value=>{   
-             const {cartOpen,cart,closeCart}=value;       
-             return (
-               <CartWrapper show={cartOpen} onClick={closeCart}>
-              <p> Cart items</p>
-               </CartWrapper>
-             )
-           }}
-         </ProductConsumer>
-        </>
-    )
+  return (
+    <div>
+      
+    </div>
+  )
 }
-
-
-
-const CartWrapper=styled.div`
-position:fixed;
-top:60px;
-right:0;
-width:100%;
-height:100%;
-background:var(--mainGrey);
-z-index:1;
-border-left:3px solid var(--primaryColor);
-transform: ${props => (props.show ? "translateX(0)" : "translateX(100%)")};
-transition:var(--mainTransition);
-@media (min-width:576px){
-  width:20rem;
-}
-`
