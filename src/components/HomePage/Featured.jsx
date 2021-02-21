11a73a70962c5788bbaae6b81ea28505613b04aa
ziaul888 <ziaul935@gmail.com>
2021-02-22 05:34:05 +0600

    featured

--- src/components/HomePage/Featured.jsx
+++ src/components/HomePage/Featured.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import Product from "../Product";
+import { Link } from "react-router-dom";
+import Title from "../Title";
+import { ProductConsumer } from "../../context";
 
 export default function Featured() {
-  return <div>Hello From Featured</div>;
+  return (
+    <section className="py-5">
+      <div className="container">
+        <Title title="featured products" center="true" />
+        <div className="row">
+          <ProductConsumer>
+            {(value) => {
+              const { featuredProducts } = value;
+              return featuredProducts.map((product) => (
+                <Product key={product.id} product={product}></Product>
+              ));
+            }}
+          </ProductConsumer>
+        </div>
+      </div>
+    </section>
+  );
 }
